c30f6e8fad19e21a574eb2cc614537b5b1697b29
Sodole <lws0915@gmail.com>
2022-12-02 16:21:20 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 # Create tests here.
 
@@ -35,6 +35,12 @@ class TestView(TestCase):
                                             author=self.user_obama)
         self.post_003.tags.add(self.tag_python_kor)
         self.post_003.tags.add(self.tag_python)
+
+        self.comment_001 = Comment.objects.create(
+            post=self.post_001,
+            author=self.user_obama,
+            content="첫 번째 댓글입니다."
+        )
 
         # 카테고리 페이지 테스트 시나리오 작성
     def test_category_page(self):
@@ -234,6 +240,11 @@ class TestView(TestCase):
         self.assertIn(self.tag_hello.name, post_area.text)
         self.assertNotIn(self.tag_python.name, post_area.text)
         self.assertNotIn(self.tag_python_kor.name, post_area.text)
+
+        comments_area = soup.find("div", id="comment-area")
+        comment_001_area = comments_area.find("div", id="comment-1")
+        self.assertIn(self.comment_001.author.username, comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
 
     def test_create_post(self):
         # 로그인하지 않으면 status code가 안된다.
