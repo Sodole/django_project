64c5ac5e35cbcdc4a6a42c7967dab868e10c4df6
Sodole <lws0915@gmail.com>
2022-12-06 11:31:41 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -78,3 +78,8 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f"{self.post.get_absolute_url()}#comment-{self.pk}"
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else :
+            return f"https://doitdjango.com/avatar/id/1359/f5e85970d9c8efa0/svg/{self.author.email}"
